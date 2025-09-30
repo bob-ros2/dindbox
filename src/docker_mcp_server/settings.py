b771c5f233168018fac3939d964d9a687b0dce61
Bob Ros <bob-ros@t-online.de>
2025-09-30 21:39:20 +0200

    switch license

--- src/docker_mcp_server/settings.py
+++ src/docker_mcp_server/settings.py
@@ -1,5 +1,5 @@
+# origin https://github.com/ckreiling/mcp-server-docker
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class ServerSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="mcp_server_")
